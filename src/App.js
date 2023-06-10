609cbcdc00f88c6f6136e3880e2c249dece345fa
stephen-ics <stephenni1234@gmail.com>
2023-06-10 13:36:28 -0400

    navbar and pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import React, { Suspense } from "react";
+import { AnimatePresence } from 'framer-motion'
+ 
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Track from './pages/Track'
+import Leaderboard from './pages/Leaderboard'
 
 function App() {
-  return (
-    <div className="App">
-      <hi className='text-red-500'>hii</hi>
-    </div>
-  );
+ return (
+   <div className="App">
+     <BrowserRouter>
+       <Suspense fallback={<div>Page Loading...</div>}>
+         <Navbar />
+         <AnimatePresence
+          mode='wait'>
+            <Routes>
+              <Route path="/" exact element={<Home />} />
+              <Route path="/track" exact element={<Track />} />
+              <Route path="/leaderboard" exact element={<Leaderboard />} />
+            </Routes>
+         </AnimatePresence>
+       </Suspense>
+     </BrowserRouter>
+   </div>
+ );
 }
-
+ 
 export default App;
