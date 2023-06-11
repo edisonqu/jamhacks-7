d3d57a84d0890b3eb99a3fcdabecceda4be403e1
edisonqu <edisonquu@gmail.com>
2023-06-11 06:12:05 -0400

    created a requirements.txt

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,9 @@ from pymongo.mongo_client import MongoClient
 from bson.objectid import ObjectId
 from datetime import datetime
 import os
+from flask import request
+import requests
+
 
 app = Flask(__name__)
 
@@ -14,13 +17,39 @@ db = client.jamhacks
 
 @app.route('/verify', methods=['POST'])
 def verification():
-    # post request that has the firebaseId, display name
-    # create a table
-    # handle the bytes to post
-    # call the azure api
-    # handle probability
+    data = request.get_json()  # get data from POST request
+    firebase_id = data.get('firebaseId')
+    display_name = data.get('displayName')
 
-    return 'Hello, World!'
+    # Create a record in MongoDB
+    collection = db['your_collection_name']  # replace with your collection name
+    doc_id = collection.insert_one({
+        "firebase_id": firebase_id,
+        "display_name": display_name,
+        "has_showered": False,  # or some other initial value
+        "probability_score": 0,  # or some other initial value
+        "time": datetime.utcnow()
+    }).inserted_id
+
+    # Handle the byte data from POST request
+    # This depends on what you're doing with the byte data
+    byte_data = data.get('byteData')
+
+    # Call the Azure API
+    # This depends on the specific Azure API you're using
+    azure_api_url = 'https://example.com/azure-api'  # replace with the actual Azure API URL
+    azure_api_response = requests.post(azure_api_url, data=byte_data)
+    azure_api_response_data = azure_api_response.json()
+
+    # Handle probability
+    # This depends on how you're handling the probability
+    probability = azure_api_response_data.get('probability')
+
+    # Update the document in MongoDB with the probability
+    collection.update_one({"_id": doc_id}, {"$set": {"probability_score": probability}})
+
+    return {"message": "Verification processed", "probability": probability}
+
 
 @app.route('/leaderboard', methods=['GET'])
 def leaderboard():
@@ -47,6 +76,15 @@ def leaderboard():
 
     results = list(collection.aggregate(pipeline))
     return jsonify(results)
+
+
+@app.route('/showers/<firebase_id>', methods=['GET'])
+def get_showers(firebase_id):
+    collection = db['showers']  # replace with your collection name
+    results = collection.find({"firebase_id": firebase_id, "has_showered": True}, {"time": 1, "_id": 0})
+    dates = [result['time'] for result in results]
+    return jsonify(dates)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
